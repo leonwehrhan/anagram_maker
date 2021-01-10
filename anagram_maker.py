a1c2fb43ba9e7045fe112e6494c9c52dc6344fe4
Leon Wehrhan <mail@leonwehrhan.de>
2021-01-10 14:42:35 +0100

    word encodings

--- anagram_maker.py
+++ anagram_maker.py
@@ -1,4 +1,39 @@
 import argparse
+import numpy as np
+import string
+import os
+
+
+def _word_to_vector(word):
+    '''
+    '''
+    v = np.zeros(26, dtype=int)
+
+    for i, c in enumerate(string.ascii_lowercase):
+        v[i] = word.count(c)
+
+    if not v.sum() == len(word):
+        raise ValueError('Only lowercase alphabetical characters allowed.')
+
+    return v
+
+
+def encode_words_alpha(file='words_alpha.txt', save=True):
+    '''
+    Encode word database as vectors.
+    '''
+    words = read_words(file)
+    encoded = np.zeros((len(words), 26), dtype=int)
+
+    for i, word in enumerate(words):
+        v = _word_to_vector(word)
+        encoded[i] = v
+
+    if save:
+        filename, file_extension = os.path.splitext(file)
+        np.save(filename + '.npy', encoded)
+
+    return encoded
 
 
 def read_words(file):
@@ -21,175 +56,5 @@ def read_words(file):
     return words
 
 
-def reduce_words(words, letters):
-    '''
-    Given the anagram search word, delete all words from list with more letters.
-
-    Parameters
-    ----------
-    words : list
-        List containing all possible words for anagram.
-    letters : str
-        Letters of the anagram.
-
-    Returns
-    -------
-    reduced_words : list
-        List not containing words with higher letter count than anagram.
-    '''
-    abc = 'abcdefghijklmnopqrstuvwxyz'
-    abc_counts = count_letters(letters)
-
-    reduced_words = []
-    for word in words:
-        if len(word) < len(letters):
-            abc_counts_word = count_letters(word)
-            if not any([abc_counts_word[key] > abc_counts[key] for key in abc]):
-                reduced_words.append(word)
-    return reduced_words
-
-
-def count_letters(word):
-    '''
-    Count letters of word and return dictionary with counts for every letter.
-
-    Parameters
-    ----------
-    word : str
-        Word whose letters will be counted.
-
-    Returns
-    -------
-    abc_counts : dict
-        Dictionary with counts for every letter in word.
-    '''
-    abc = 'abcdefghijklmnopqrstuvwxyz'
-    abc_counts = {}
-    for letter_alpha in abc:
-        count = 0
-        for letter_word in word:
-            if letter_word == letter_alpha:
-                count += 1
-        abc_counts[letter_alpha] = count
-    return abc_counts
-
-
-def get_len_indices(wordlist):
-    '''
-    Given a list of words with varying lengths, return a dictionary that maps
-    word lengths to the index of the last word with that length in the list.
-
-    Parameters
-    ----------
-    wordlist : list
-        List of words. Has to be sorted by length.
-
-    Returns
-    -------
-    di : dict
-        Dictionary that maps word lengths as keys to the index of the last word
-        in wordlist with that length as value.
-    '''
-    di = {}
-    length = 1
-
-    for i, w in enumerate(wordlist):
-        length_word = len(w)
-
-        # verify list is sorted
-        if length_word > length:
-
-            # set index of last word of previous length as value of that length
-            length = length_word
-            di[length_word - 1] = i
-
-        # raise error if not sorted
-        elif length_word < length:
-            raise ValueError('Word list not sorted.')
-
-    # fill missing keys in dictionary
-    for key in range(1, length):
-        if key not in di:
-
-            # set value to that of previous length
-            di[key] = di[key - 1]
-
-    # fill up with keys for lengths within max. length from wordlist + 11
-    for key in range(length, length + 11):
-
-        # set value to that of maximum from wordlist
-        di[key] = di[length - 1]
-
-    # word with length 0
-    di[0] = None
-
-    return di
-
-
-def find_anagrams(words, word1):
-    '''
-    Find anagrams of a search word from a list of words.
-
-    Parameters
-    ----------
-    words : list
-        List of words from which anagrams will be made.
-    word1 : str
-        Anagram search word.
-
-    Returns
-    -------
-    anagrams : list
-        Anagrams of the search word. The anagrams can be 1-word or 2-word
-        combinations.
-    '''
-    # list for 1-word anagrams
-    w1_anagrams = []
-
-    # list for 2 word anagrams
-    w2_anagrams = []
-
-    # words that will be considered for 2-word combinations
-    words_for_w2 = []
-
-    # search wordlist for 1-word anagrams
-    for word in words:
-        if len(word1) == len(word):
-            if count_letters(word1) == count_letters(word):
-                w1_anagrams.append(word)
-
-        # word from list only considered for 2-word anagrams if shorter
-        elif len(word) < len(word1):
-            words_for_w2.append(word)
-
-        # no need to continue if word from list is longer, list must be sorted
-        elif len(word) > len(word1):
-            break
-
-    lens_words_w2 = get_len_indices(words_for_w2)
-
-    # search wordlist for 2-word anagrams
-    for word in words_for_w2:
-        # only considered if length of both is equal to word1
-        for word2 in words_for_w2[lens_words_w2[len(word1) - len(word) - 1]:
-                                  lens_words_w2[len(word1) - len(word)]]:
-            comb = word + word2
-            if len(comb) == len(word1):
-                if count_letters(word1) == count_letters(comb):
-                    w2_anagrams.append(word + f' {word2}')
-
-    return w1_anagrams + w2_anagrams
-
-
 if __name__ == '__main__':
-    words = read_words('words_alpha.txt')
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('query')
-    args = parser.parse_args()
-
-    reduced_words = reduce_words(words, args.query)
-    anagrams = find_anagrams(reduced_words, args.query)
-
-    for a in anagrams:
-        print(a)
+    encode_words_alpha()
